133aa1466c87c1c54ca4b82e2421adc2f181d2ed
Erdenetsogt <erdenetsogt@gmail.com>
2024-11-15 00:15:28 +0800

    change port

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app: Express = express();
 const prisma = new PrismaClient();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
